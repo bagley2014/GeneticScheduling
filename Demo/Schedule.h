79e6224a5c49d263e0d1f5cc990b08c623984bff
bagley2014 <dbagley@harding.edu>
2018-04-12 11:45:02 -0500

    Added the demo version again

---
+++ Demo/Schedule.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <bitset>
+#include "Course.h"
+
+class Schedule
+{
+public:
+	Schedule();
+	Schedule(int);
+
+	static const int COURSE_COUNT = 42;
+	
+	static void crossover(const Schedule&, const Schedule&, Schedule&, Schedule&);
+
+	__int8 operator[](int);
+	bool operator<(const Schedule) const;
+
+	void mutate();
+	std::string test();
+	std::string dataString();
+private:
+	std::bitset<COURSE_COUNT * 3> data; 
+};
+
